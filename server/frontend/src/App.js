b00b86566221234190ddcb12795a234e79940663
ai-for-all-coders <ask.aiforall@gmail.com>
2026-01-05 13:23:24 -0500

    Capstone progress: auth, frontend routes, contact, about pages

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,22 @@
-import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <Routes>
+      {/* Home page */}
+      <Route path="/" element={<Home />} />
+
+      {/* Login page */}
       <Route path="/login" element={<LoginPanel />} />
+
+      {/* Register page */}
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
+
 export default App;
