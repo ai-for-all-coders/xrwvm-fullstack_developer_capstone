8accc3924e8c415e2348a145a9f4fb76bed24d44
ai-for-all-coders <ask.aiforall@gmail.com>
2026-01-06 00:52:28 -0500

    changes merged

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,19 +1,12 @@
-import { Routes, Route } from "react-router-dom";
-
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import Home from "./components/Home/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      {/* Home page */}
-      <Route path="/" element={<Home />} />
-
-      {/* Login page */}
+      <Route path="/" element={<LoginPanel />} />
       <Route path="/login" element={<LoginPanel />} />
-
-      {/* Register page */}
       <Route path="/register" element={<Register />} />
     </Routes>
   );
